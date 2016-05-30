f00c81c4606714349fe0215bd30f348ad7102a85
Roman Lisovskiy <rolisovskiy@mail.ru>
2016-05-30 20:48:16 +0300

    testing

--- ebuilders2/src/js/partials/app.js
+++ ebuilders2/src/js/partials/app.js
@@ -1,4 +1,46 @@
+//slider
+(function($) {
+    var $sliderUL = $('div.slider').css('overflow', 'hidden').children('ul'),
+        $imgs = $sliderUL.find('img'),
+        $imgWidth = $imgs[0].width,
+        $imgsLen = $imgs.length,
+        $current = 1,
+        $totalImgsWidth = $imgsLen * $imgWidth;
 
+    $('#slider-nav').show().find('button').on('click', function(e) {
+        e.preventDefault();
+        var $direction = $(this).data('dir'),
+            $loc = $imgWidth;
+
+
+        ( $direction === 'next' ) ? ++$current : --$current;
+
+        if ( $current === 0 ) {
+            $current = $imgsLen;
+            $loc = $totalImgsWidth - $imgWidth;
+            $direction = 'next';
+        } else if ( $current - 1 === $imgsLen ) {
+            $current = 1;
+            $loc = 0;
+        }
+
+        transition($sliderUL, $loc, $direction);
+    });
+
+    function transition( $container, $loc, $direction ) {
+        var $unit;
+
+        if ( $direction && $loc !== 0 ) {
+            $unit = ( $direction === 'next' ) ? '-=' : '+=';
+        }
+
+        $container.animate({
+            'margin-left': $unit ? ($unit + $loc) : $loc
+
+        });
+    }
+
+})(jQuery);
 $(document).ready(function() {
 
     var $bannerImg = $('.banner__img'),
@@ -124,49 +166,7 @@ $(document).ready(function() {
 
 });
 
-//slider
-(function($) {
-    var $sliderUL = $('div.slider').css('overflow', 'hidden').children('ul'),
-        $imgs = $sliderUL.find('img'),
-        $imgWidth = $imgs[0].width,
-        $imgsLen = $imgs.length,
-        $current = 1,
-        $totalImgsWidth = $imgsLen * $imgWidth;
 
-    $('#slider-nav').show().find('a').on('click', function(e) {
-        e.preventDefault();
-        var $direction = $(this).data('dir'),
-            $loc = $imgWidth;
-
-
-        ( $direction === 'next' ) ? ++$current : --$current;
-
-        if ( $current === 0 ) {
-            $current = $imgsLen;
-            $loc = $totalImgsWidth - $imgWidth;
-            $direction = 'next';
-        } else if ( $current - 1 === $imgsLen ) {
-            $current = 1;
-            $loc = 0;
-        }
-
-        transition($sliderUL, $loc, $direction);
-    });
-
-    function transition( $container, $loc, $direction ) {
-        var $unit;
-
-        if ( $direction && $loc !== 0 ) {
-            $unit = ( $direction === 'next' ) ? '-=' : '+=';
-        }
-
-        $container.animate({
-            'margin-left': $unit ? ($unit + $loc) : $loc
-
-        });
-    }
-
-})(jQuery);
 
 
 
