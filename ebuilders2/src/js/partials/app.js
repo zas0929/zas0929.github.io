1dd983d014359718ec514d4e0d0aabba918e88fe
Roman Lisovskiy <rolisovskiy@mail.ru>
2016-05-30 20:05:59 +0300

    test

--- ebuilders2/src/js/partials/app.js
+++ ebuilders2/src/js/partials/app.js
@@ -1,48 +1,47 @@
+(function($) {
+    var $sliderUL = $('div.slider').css('overflow', 'hidden').children('ul'),
+        $imgs = $sliderUL.find('img'),
+        $imgWidth = $imgs[0].width,
+        $imgsLen = $imgs.length,
+        $current = 1,
+        $totalImgsWidth = $imgsLen * $imgWidth;
 
-$(document).ready(function() {
-    //slider
-    (function($) {
-        var $sliderUL = $('div.slider').css('overflow', 'hidden').children('ul'),
-            $imgs = $sliderUL.find('img'),
-            $imgWidth = $imgs[0].width,
-            $imgsLen = $imgs.length,
-            $current = 1,
-            $totalImgsWidth = $imgsLen * $imgWidth;
-
-        $('#slider-nav').show().find('a').on('click', function(e) {
-            e.preventDefault();
-            var $direction = $(this).data('dir'),
-                $loc = $imgWidth;
+    $('#slider-nav').show().find('a').on('click', function(e) {
+        e.preventDefault();
+        var $direction = $(this).data('dir'),
+            $loc = $imgWidth;
 
 
-            ( $direction === 'next' ) ? ++$current : --$current;
+        ( $direction === 'next' ) ? ++$current : --$current;
 
-            if ( $current === 0 ) {
-                $current = $imgsLen;
-                $loc = $totalImgsWidth - $imgWidth;
-                $direction = 'next';
-            } else if ( $current - 1 === $imgsLen ) {
-                $current = 1;
-                $loc = 0;
-            }
-
-            transition($sliderUL, $loc, $direction);
-        });
-
-        function transition( $container, $loc, $direction ) {
-            var $unit;
-
-            if ( $direction && $loc !== 0 ) {
-                $unit = ( $direction === 'next' ) ? '-=' : '+=';
-            }
-
-            $container.animate({
-                'margin-left': $unit ? ($unit + $loc) : $loc
-
-            });
+        if ( $current === 0 ) {
+            $current = $imgsLen;
+            $loc = $totalImgsWidth - $imgWidth;
+            $direction = 'next';
+        } else if ( $current - 1 === $imgsLen ) {
+            $current = 1;
+            $loc = 0;
         }
 
-    })(jQuery);
+        transition($sliderUL, $loc, $direction);
+    });
+
+    function transition( $container, $loc, $direction ) {
+        var $unit;
+
+        if ( $direction && $loc !== 0 ) {
+            $unit = ( $direction === 'next' ) ? '-=' : '+=';
+        }
+
+        $container.animate({
+            'margin-left': $unit ? ($unit + $loc) : $loc
+
+        });
+    }
+
+})(jQuery);
+$(document).ready(function() {
+
     var $bannerImg = $('.banner__img'),
         $banner = $('.banner'),
         $bannerTitle = $('.banner__title'),
@@ -165,6 +164,7 @@ $(document).ready(function() {
     loadData();
 
 });
+//slider
 
 
 
