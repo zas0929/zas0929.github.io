2fe6dcfd2fa791d2479f2ac10684dac4306d5182
Roman <rolisovskiy@mail.ru>
2016-06-30 16:46:15 +0300

    changes length text

--- projecthabr/src/js/partials/app.js
+++ projecthabr/src/js/partials/app.js
@@ -6,7 +6,7 @@ var inputCont = $('.cont').text();
 var inputAttr = $('input').val();
 var re = /.*\..*/gi;
 var inputAttrCut = inputAttr.match(re)
-    $('.cont').html(inputAttrCut);
+    $('.cont').text(inputAttrCut);
   })
 
 });
