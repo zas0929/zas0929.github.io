b8eb576bb9a3f4831bc27e6f7f6bbb32bb52b4ad
Roman <rolisovskiy@mail.ru>
2016-08-29 15:50:40 +0300

    testing

--- apixio/src/js/partials/app.js
+++ apixio/src/js/partials/app.js
@@ -15,5 +15,10 @@ $(document).ready(function() {
   $('.slicknav_arrow').on('click', function() {
     $('.sub-menu').slideToggle(200);
     $('.slicknav_arrow').toggleClass('rotate-arrow');
-  })
+  });
+
+  //other dots
+  $('.slick-dots li button').replaceWith(function(index, oldHTML){
+  return $("<span>").html(oldHTML);
+});
 });
