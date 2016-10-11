b0fa2f8fef15b54be168bf9cef5c45069e7f81c9
Roma-L <ghost850@gmail.com>
2016-10-11 16:52:15 +0300

    days timer

--- Imt/Roma-L/timer-interval/script.js
+++ Imt/Roma-L/timer-interval/script.js
@@ -1,20 +1,67 @@
 'use strict'
 
+var now = new Date();
+// alert(now.getFullYear() + ' ' + now.getHours());
 
-var intId, intId2, i = 0, j = 0;
+var then = new Date(2016, 11, 31, 0, 0, 0);
+var time = (then - now)/1000,
+	days = parseInt(time/ 86400),
+	hours = time%86400;
 
-intId = setInterval(function() {
-	console.log('hello');
-	i++;
-	if(i == 5) {
-		clearInterval(intId)
-	}
-}, 1000);
 
-intId2 = setInterval(function() {
-	console.log('bue');
-	j++;
-	if(j == 5) {
-		clearInterval(intId2)
-	}
-}, 1000);
+	alert(hours)
+
+// $(document).ready(function() {
+// 	var countTimer = parseInt($('#count').text()),
+// 		startBtn = $('#start'),
+// 		stopBtn = $('#stop');
+
+// 		var intId = false;
+// 		function startTimer() {
+// 			intId = setInterval(function() {
+// 				countTimer = countTimer + 1;
+// 				$('#count').text(countTimer);
+// 			}, 1000)
+// 		}
+// 		function stopTimer() {
+// 			clearInterval(intId)
+// 		}
+// 		startBtn.on('click', function() {
+// 			startTimer();
+// 			console.log(intId);
+// 		});
+
+// 		stopBtn.on('click', function() {
+// 			stopTimer();
+// 		})
+
+
+
+	
+	
+		
+	
+// });
+
+
+// var intId, intId2, i = 0, j = 0;
+
+// intId = setInterval(function() {
+// 	console.log('hello');
+// 	i++;
+// 	if(i == 5) {
+// 		clearInterval(intId)
+// 	}
+// }, 1000);
+
+// intId2 = setInterval(function() {
+// 	console.log('bue');
+// 	j++;
+// 	if(j == 5) {
+// 		clearInterval(intId2)
+// 	}
+// }, 1000);
+
+// var timerId = setTimeout(function() {
+// 	alert();
+// }, 1000);
